8e1ae1142118156110fdd444b9f402ef000f358d
Antonio Estela <antonioestela73@gmail.com>
2021-05-29 12:28:05 -0500

    First component

---
+++ src/components/Badge.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Badge () {
+    return (
+        <div className="badge">
+            <div className="badge__header">
+                <img src="" alt="header logo"/>
+            </div>
+            <div className="badge__content"></div>
+            <div className="badge__footer">#platziconf</div>
+        </div>
+    );
+}
+
+export default Badge;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-const container = document.getElementById('app');
+import Badge from './components/Badge';
 
-const element = <h1>Hello, Platzi Badges</h1>;
+// React == Javascript
+// JSX is a mix between HTML, CSS and javascript that is interpreted before it's send it to the browser
+
+// We can write HTML syntax thanks to JSX wich is imported with React
+const element = (
+  <Badge />
+);
+
+// A great feature that JSX offers is to use javascript expressions into HTML code
+// Example:
+// const name = 'Antonio';
+// <p>Hi, my name is {name}</p>
+// this will render the value of name into the <p> tag
+
+// Another way to create an element is using React.createElement
+// const elementReact = React.createElement(
+//   "h1",
+//   {},
+//   "Hola, Soy un elemento creado con React.createElement"
+// );
+
+const container = document.getElementById("app");
+
+// ReactDOM.render will render the element that we passed through inside the container.
 ReactDOM.render(element, container);
