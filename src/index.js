6d2258bc03b9e944ec414c7991c9157840e9fa7f
Antonio Estela <antonioestela73@gmail.com>
2021-06-23 12:53:34 -0500

    New page has been added

--- src/index.js
+++ src/index.js
@@ -2,29 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import BadgeNew from "./pages/BadgeNew/BadgeNew.js";
+import Badges from "./pages/Badges/Badges.js";
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 
-// React == Javascript
-// JSX is a mix between HTML, CSS and javascript that is interpreted before it's send it to the browser
 
-// We can write HTML syntax thanks to JSX wich is imported with React
-const element = <BadgeNew />;
-
-// A great feature that JSX offers is to use javascript expressions into HTML code
-// Example:
-// const name = 'Antonio';
-// <p>Hi, my name is {name}</p>
-// this will render the value of name into the <p> tag
-
-// Another way to create an element is using React.createElement
-// const elementReact = React.createElement(
-//   "h1",
-//   {},
-//   "Hola, Soy un elemento creado con React.createElement"
-// );
+//const element = <BadgeNew />;
 
 const container = document.getElementById("app");
 
 // ReactDOM.render will render the element that we passed through inside the container.
-ReactDOM.render(element, container);
+ReactDOM.render(<Badges />, container);
